d5b4663a6f847ad91b14f2da4c07c97bafbb7cd8
kj20207 <kj20207@iastate.edu>
2017-01-17 20:05:19 -0600

    Min and Max
    Places first two items of parsed list as min and max.

--- Earth_Modeling_Parser/src/Earth_Modeling_Parser/Parser.java
+++ Earth_Modeling_Parser/src/Earth_Modeling_Parser/Parser.java
@@ -8,16 +8,22 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Parser {
 
 	public static void main(String args[]) throws IOException{
-		BufferedReader f = new BufferedReader(new FileReader("ch4y2001m2.txt"));
+		
+		
+		String file_to_parse = "ch4y2001m5";
+		ArrayList<String> lines = new ArrayList<String>();
+		
+		BufferedReader f = new BufferedReader(new FileReader("Original_ASCII_files\\" + file_to_parse + ".txt"));
 		
 		Scanner scanner;
 		try {
-			PrintWriter output = new PrintWriter(new BufferedWriter(new FileWriter("generated_values.csv")));
+			PrintWriter output = new PrintWriter(new BufferedWriter(new FileWriter("Parsed_CSVs\\" + file_to_parse+ ".csv")));
 			output.println("latitude,longitude,value");
 			scanner = new Scanner(f);
 			
@@ -49,13 +55,12 @@ public class Parser {
 			scanheaders = new Scanner(headers);
 			scanheaders.next();
 			double cellsize = scanheaders.nextDouble();
-			//cellsize = 0.08333333333;
+
 			headers = scanner.nextLine();
 			headers = scanner.nextLine();
 			scanheaders = new Scanner(headers);
 			scanheaders.next();
 			double NODATA_value = scanheaders.nextDouble();
-			//NODATA_value = 0.083333333333;
 			scanheaders.close();
 			
 			System.out.println(ncols + " ");
@@ -64,13 +69,35 @@ public class Parser {
 			double latitude = yllcorner + (cellsize * (nrows - 1));
 			int rows = 0;
 			int columns = 0;
+			double max = 2017;
+			double min = 2017;
+			
 			while(scanner.hasNextLine()){
 				String scanning = scanner.nextLine();
 				Scanner linescan = new Scanner(scanning);
 				while(linescan.hasNext()){
 					double value = linescan.nextDouble();
 					if(value != NODATA_value){
-					output.println((latitude - rows * cellsize)+ "," + (longitude + columns * cellsize)  +"," + value);
+						if(max == 2017 && min == 2017){
+							max = value;
+							min = value;
+							lines.add((latitude - rows * cellsize)+ "," + (longitude + columns * cellsize)  +"," + value);
+						}
+						else if(value > max){
+							max = value;
+							lines.add(1, (latitude - rows * cellsize)+ "," + (longitude + columns * cellsize)  +"," + value);
+						}
+						else if (value < min){
+							min = value;
+							lines.add(0, (latitude - rows * cellsize)+ "," + (longitude + columns * cellsize)  +"," + value);
+							if(lines.size() > 2){
+								String temp = lines.remove(2);
+								lines.add(1, temp);
+							}
+						}else{
+					        lines.add((latitude - rows * cellsize)+ "," + (longitude + columns * cellsize)  +"," + value);
+						}
+						
 					}
 					columns++;
 					if(columns % 1404 == 0){
@@ -83,8 +110,14 @@ public class Parser {
 				}
 				linescan.close();
 			}
-			//System.out.println(rows + " " + columns);
-			//System.out.println(counter);
+			
+			while(!lines.isEmpty()){
+				output.println(lines.remove(0));
+			}
+			
+			// Print out Max and Min (TESTING PURPOSES)
+			//System.out.println(max + "   " + min);
+
 			output.close();
 			scanner.close();
 		} catch (FileNotFoundException e) {
