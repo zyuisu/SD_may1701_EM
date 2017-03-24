cf6923fc8780887badac0e4d3ea6dc19b1dc30a7
epdevine <epdevine@iastate.edu>
2017-03-24 00:41:07 -0500

    Fixed Month Issue

--- Earth_Modeling_Parser/src-server/parser/JavaScriptGenerator.java
+++ Earth_Modeling_Parser/src-server/parser/JavaScriptGenerator.java
@@ -270,7 +270,7 @@ public class JavaScriptGenerator {
 			strBuff.append("switch (");
 			strBuff.append(yearArrays.get(index));
 			strBuff.append(
-					"[i]){ default: break; case '0': monthName = 'January'; case '1': monthName = 'February'; case '2': monthName = 'March'; case '3': monthName = 'April'; case '4': monthName = 'May'; case '5': monthName = 'June'; case '6': monthName = 'July'; case '7': monthName = 'August'; case '8': monthName = 'September'; case '9': monthName = 'October'; case '10': monthName = 'November'; case '11': monthName = 'December'; ");
+					"[i]){ default: break; case '0': monthName = 'January'; break; case '1': monthName = 'February'; break; case '2': monthName = 'March'; break; case '3': monthName = 'April'; break; case '4': monthName = 'May'; break; case '5': monthName = 'June'; break; case '6': monthName = 'July'; break; case '7': monthName = 'August'; break; case '8': monthName = 'September'; break; case '9': monthName = 'October'; break; case '10': monthName = 'November'; break; case '11': monthName = 'December'; break; ");
 			strBuff.append("} monthList[i+1] = new Option(");
 			strBuff.append("monthName,");
 			strBuff.append(yearArrays.get(index));
